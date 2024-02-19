9eed94724d2556e7f2c0400dc9b5f5625ae11d67
hello <hellohoangson@gmail.com>
2024-02-19 16:40:24 +0700

    Continue completing Category and Product Api

--- nhs-api/src/main/java/com/nhs/individual/Controller/CartController.java
+++ nhs-api/src/main/java/com/nhs/individual/Controller/CartController.java
@@ -1,13 +1,11 @@
 package com.nhs.individual.Controller;
 
+import com.nhs.individual.Domain.Cart;
 import com.nhs.individual.Domain.CartItem;
 import com.nhs.individual.Service.CartItemService;
 import com.nhs.individual.Service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -17,19 +15,32 @@ public class CartController {
     CartService cartService;
     @Autowired
     CartItemService cartItemService;
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public Collection<CartItem> findAllByUserId(@RequestParam(value = "userId",required=true) Integer userId){
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Collection<CartItem> findAllByUserId(@RequestParam(value = "id",required=true) Integer userId){
         return cartItemService.findAllByCartId(userId);
     }
-//    @RequestMapping(value = "/",method = RequestMethod.POST)
-//    public CartItem create(@RequestBody CartItem cartItem,
-//                             @RequestParam(value = "userId",required=true) Integer userId){
-//        return cartItemService.addItem(userId,cartItem);
-//    }
-//    @RequestMapping(value = "/",method = RequestMethod.DELETE)
-//    public void delete(@RequestParam(value = "id") Integer id){
-//        cartItemService.deleteById(id);
-//    }
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public Cart updateById(@PathVariable(value = "id") Integer id,
+                                           @RequestBody Cart cart
+    ){
+        return cartService.update(id,cart);
+    }
+    @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+    public void deleteById(@PathVariable(value = "id") Integer id){
+        cartService.deleteById(id);
+    }
+    //CartItem
+    @RequestMapping(value = "/{cart_id}/item",method = RequestMethod.POST)
+    public CartItem create(@RequestBody CartItem cartItem,
+                             @RequestParam(value = "cart_id",required=true) Integer cartId){
+        return cartItemService.addItem(cartId,cartItem);
+    }
+    @RequestMapping(value = "/{cart_id}/item/{item_id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "cart_id",required = false) Integer cart_id,
+                       @PathVariable(value = "item_id") Integer item_id){
+        cartItemService.deleteById(item_id);
+    }
+
 //    @RequestMapping(value = "/",method = RequestMethod.PUT)
 //    public CartItem update(@RequestBody CartItem cartItem,
 //                             @RequestParam(value = "ud",required=true) Integer id){
--- nhs-api/src/main/java/com/nhs/individual/Service/CartService.java
+++ nhs-api/src/main/java/com/nhs/individual/Service/CartService.java
@@ -1,7 +1,9 @@
 package com.nhs.individual.Service;
 
 import com.nhs.individual.Domain.Cart;
+import com.nhs.individual.Exception.ResourceNotFoundException;
 import com.nhs.individual.Repository.CartRepository;
+import com.nhs.individual.Utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,11 @@ public class CartService {
     }
     public Optional<Cart> findById(int id){
         return cartRepository.findById(id);
+    }
+    public Cart update(Integer id,Cart cart){
+        return cartRepository.findById(id).map(oldCart->{
+            return cartRepository.save(ObjectUtils.merge(oldCart,cart,Cart.class));
+        }).orElseThrow(()->new ResourceNotFoundException("Cart not found"));
     }
     public void deleteById(int id){
         cartRepository.deleteById(id);
